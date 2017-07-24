a812cf1c5ecaaf1e888b8f1e778c59a28d644280
oxomichael <oxomichael@hotmail.com>
2017-07-24 08:32:40 +0200

    Add CarbuBundle

---
+++ src/CarbuBundle/Entity/Full.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace CarbuBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Full
+ *
+ * @ORM\Table(name="full")
+ * @ORM\Entity(repositoryClass="CarbuBundle\Repository\FullRepository")
+ */
+class Full
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="quantity", type="decimal", precision=2, scale=2)
+     */
+    private $quantity;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="price", type="decimal", precision=3, scale=2)
+     */
+    private $price;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="distance", type="integer")
+     */
+    private $distance;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Full
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set quantity
+     *
+     * @param string $quantity
+     *
+     * @return Full
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    /**
+     * Get quantity
+     *
+     * @return string
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     *
+     * @return Full
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set distance
+     *
+     * @param integer $distance
+     *
+     * @return Full
+     */
+    public function setDistance($distance)
+    {
+        $this->distance = $distance;
+
+        return $this;
+    }
+
+    /**
+     * Get distance
+     *
+     * @return int
+     */
+    public function getDistance()
+    {
+        return $this->distance;
+    }
+}
+
---
+++ src/CarbuBundle/Entity/Vehicle.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace CarbuBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Vehicle
+ *
+ * @ORM\Table(name="vehicle")
+ * @ORM\Entity(repositoryClass="CarbuBundle\Repository\VehicleRepository")
+ */
+class Vehicle
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="fuel_capacity", type="integer")
+     */
+    private $fuelCapacity;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="power", type="integer")
+     */
+    private $power;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Vehicle
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set fuelCapacity
+     *
+     * @param integer $fuelCapacity
+     *
+     * @return Vehicle
+     */
+    public function setFuelCapacity($fuelCapacity)
+    {
+        $this->fuelCapacity = $fuelCapacity;
+
+        return $this;
+    }
+
+    /**
+     * Get fuelCapacity
+     *
+     * @return int
+     */
+    public function getFuelCapacity()
+    {
+        return $this->fuelCapacity;
+    }
+
+    /**
+     * Set power
+     *
+     * @param integer $power
+     *
+     * @return Vehicle
+     */
+    public function setPower($power)
+    {
+        $this->power = $power;
+
+        return $this;
+    }
+
+    /**
+     * Get power
+     *
+     * @return int
+     */
+    public function getPower()
+    {
+        return $this->power;
+    }
+}
+
